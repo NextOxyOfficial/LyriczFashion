2a6161804756e378fd00d577d2b923f7419a71de
Alimul Islam <alimulislam50@gmail.com>
2026-02-22 04:48:56 -0800

    Fix: disable DRF browsable HTML renderer to prevent Django API page showing on direct URL visit

--- backend/lyriczfashion/settings.py
+++ backend/lyriczfashion/settings.py
@@ -138,6 +138,12 @@ JAZZMIN_UI_TWEAKS = {
     },
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
